1b967c5eebf7dde8ef3693076bf2a6f6822e2175
coder-shj <1790903797@qq.com>
2020-11-23 22:58:24 +0800

    vue-axios的学习

--- src/network/request.js
+++ src/network/request.js
@@ -3,12 +3,39 @@ import axios from 'axios'
 
 // 方案四
 export function request(config) {
+  // 1.创建axios的实例
   const instance = axios.create({
     baseURL: 'http://152.136.185.210:8000/api/w6',
     timeout: 5000
   })
 
-  // f发送真正的网络请求
+  // 2.axios的拦截器
+  // 请求的拦截器  use内有两参数: 请求成功和请求失败(请求失败一般不会出现)
+  instance.interceptors.request.use(config => {
+    // console.log(config);
+    // 拦截器的作用
+    // 1.比如config中的一些信息不符合服务器的要求
+
+    // 2.比如每次发送网络请求时,都希望在界面中显示一个请求的图标
+
+    // 3.某些网络请求(比如登录(token)),必须携带一些特殊的信息
+
+    // 拦截后需要return,不然会到catch中
+    return config
+  },err => {
+    console.log(err);
+  })
+  // 响应的拦截器
+  instance.interceptors.response.use(res => {
+    console.log(res);//拿到的是请求的结果信息
+
+    // 拦截后需要return出最终的信息,如果不返回的话,最终获取的为undefined
+    return res.data
+  },err => {
+    console.log(err);
+  })
+
+  // 3.发送真正的网络请求
   return instance(config)
 }
 
@@ -21,7 +48,7 @@ export function request(config) {
 //       timeout: 5000
 //     })
 
-//     // f发送真正的网络请求
+//     // 发送真正的网络请求
 //     instance(config)
 //       .then(res => {
 //         resolve(res)
@@ -40,7 +67,7 @@ export function request(config) {
 //     timeout: 5000
 //   })
 
-//   // f发送真正的网络请求
+//   // 发送真正的网络请求
 //   instance(config.baseConfig)
 //     .then(res => {
 //       config.success(res);
@@ -58,7 +85,7 @@ export function request(config) {
 //     timeout: 5000
 //   })
 
-//   // f发送真正的网络请求
+//   // 发送真正的网络请求
 //   instance(config)
 //     .then(res => {
 //       success(res);
